80bda5d45c0c90c26a62ff9f96dc471d32f0ae78
Muhammet Kır <muhammetkir97@gmail.com>
2021-11-13 16:01:33 +0300

    Initial commit

---
+++ Assets/Scripts/ControllerSystem/ControllerSystem.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+interface IControllerSystem
+{
+    void Init();
+
+}
+
+public class ControllerSystem : MonoBehaviour
+{
+    #region Singleton
+    public static ControllerSystem  Instance {get; private set;}
+
+    #endregion
+
+
+    Dictionary<string,float> AxisList = new Dictionary<string, float>();
+
+
+    void Awake()
+    {
+        if(Instance == null)
+        {
+            Instance = this;
+            Invoke("InitializeControllers",0.2f);
+
+    
+            //DontDestroyOnLoad(gameObject);
+
+        }
+    }
+
+    void InitializeControllers()
+    {
+        var objects = FindObjectsOfType<GameObject>();
+        foreach(GameObject obj in objects)
+        {
+            if(obj.GetComponent<IControllerSystem>() != null)
+            {
+                obj.GetComponent<IControllerSystem>().Init();
+            }
+        }
+    }
+
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void CreateAxis(string AxisName)
+    {
+        if(AxisList.ContainsKey(AxisName))
+        {
+            Debug.Log("Axis Already Exist!");
+        }
+        else
+        {
+            AxisList.Add(AxisName,0);
+        }
+
+    }
+
+    public void SetAxisValue(string AxisName, float AxisValue)
+    {
+        if(AxisList.ContainsKey(AxisName))
+        {
+            AxisList[AxisName] = AxisValue;
+        }
+        else
+        {
+            Debug.Log("!Axis Not Exist!");
+        }
+
+    }
+
+
+
+    public float GetAxis(string AxisName)
+    {
+        float axisValue = 0;
+        if(AxisList.ContainsKey(AxisName))
+        {
+            axisValue = AxisList[AxisName];
+        }
+        else
+        {
+            Debug.Log("!Axis Not Exist!");
+        }
+
+        return axisValue;
+    }
+}
---
+++ Assets/Scripts/RobotCleaner.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RobotCleaner : MonoBehaviour
+{
+    private RobotStatus robotStatus = RobotStatus.Working;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 movementDirection = new Vector3(ControllerSystem.Instance.GetAxis("Horizontal"), 0, ControllerSystem.Instance.GetAxis("Vertical"));
+        transform.Translate(movementDirection * 0.1f);
+    }
+
+    public RobotStatus GetRobotStatus()
+    {
+        return robotStatus;
+    }
+}
+
+
+public enum RobotStatus
+{
+    Working,
+    Stopped
+}
